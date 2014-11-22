b7b3b31331c7a1fe26c3e3558198b338768cc2ac
Robert Siebert <rsiebert@miroku.no-ip.com>
2014-11-23 00:09:31 +0100

    Added some comments.

--- Settings.pm
+++ Settings.pm
@@ -12,10 +12,16 @@ use base qw(Slim::Web::Settings);
 
 use Slim::Utils::Prefs;
 
+# Returns the name of the plugin. The real 
+# string is specified in the strings.txt file.
 sub name {
 	return 'PLUGIN_SQUEEZECLOUD';
 }
 
+# The path points to the HTML page that is used to set the plugin's settings.
+# The HTML page is in some funky HTML-like format that is used to display the 
+# settings page when you select "Settings->Extras->[plugin's settings box]" 
+# from the SC7 window.
 sub page {
 	return 'plugins/SqueezeCloud/settings/basic.html';
 }
@@ -24,4 +30,5 @@ sub prefs {
 	return (preferences('plugin.squeezecloud'), qw(apiKey playmethod));
 }
 
+# Always end with a 1 to make Perl happy
 1;
