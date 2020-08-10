13c783eae663459bab4b0f28341c2d3f2944925d
Daniel Vijge <danielvijge@users.noreply.github.com>
2020-08-10 17:57:50 +0200

    Make versions number work for auto upgrade in LMS

--- generate-zip.sh
+++ generate-zip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-VERSION=`git describe --tags`
+VERSION=`git describe --tags --abbrev=0`.`git rev-list $(git describe --tags --abbrev=0)..HEAD --count`
 
 sed "s/{{ env\['VERSION'\] }}/$VERSION/g" install.template.xml > install.xml
 zip -r SqueezeCloud-$VERSION.zip . -x \*.zip \*.sh \*.git\* \*README\* \*webauth\* \*.sublime\* \*.DS_Store\* \*.editorconfig \*.template.xml
